757c018e75de63d207059b4d86413e40c1fb13e8
aleksandar.naydenov <aleksandar.naydenov@delta.bg>
2025-10-10 15:56:15 +0300

    Refactor ParserConstructor trait to enhance type handling in constructor

--- src/Traits/ParserConstructor.php
+++ src/Traits/ParserConstructor.php
@@ -5,32 +5,66 @@ declare(strict_types=1);
 namespace Aon4o\Cs2GsiParser\Traits;
 
 use ReflectionClass;
+use ReflectionNamedType;
 
 trait ParserConstructor
 {
     public function __construct(object $data)
     {
-        $reflection = new ReflectionClass(self::class);
+        $reflection = new ReflectionClass($this);
 
         foreach ($reflection->getProperties() as $property) {
             $name = $property->getName();
-            $type = $property->getType()?->getName();
-            $value = $data->{$name};
-            echo "$name: $type\n";
+            $type = $property->getType();
 
-            if (enum_exists($type)) {
-                if ($value !== null) {
-                    $this->{$name} = $type::from($value);
-                }
+            if (! (property_exists($data, $name) || isset($data->{$name}))) {
+                continue;
+            }
+
+            $value = $data->{$name} ?? null;
+
+            if ($type === null) {
+                $property->setValue($this, $value);
 
                 continue;
             }
 
-            if (class_exists($type)) {
-                $this->{$name} = new $type($value);
-            }
-        }
+            if ($type instanceof ReflectionNamedType) {
+                $type_name = $type->getName();
 
-        return $this;
+                if ($value === null) {
+                    $property->setValue($this, null);
+
+                    continue;
+                }
+
+                if (enum_exists($type_name)) {
+                    $property->setValue($this, $type_name::from($value));
+
+                    continue;
+                }
+
+                if (class_exists($type_name)) {
+                    $property->setValue($this, new $type_name($value));
+
+                    continue;
+                }
+
+                if ($type->isBuiltin()) {
+                    match ($type_name) {
+                        'int' => $property->setValue($this, (int) $value),
+                        'float' => $property->setValue($this, (float) $value),
+                        'bool' => $property->setValue($this, (bool) $value),
+                        'string' => $property->setValue($this, (string) $value),
+                        'array' => $property->setValue($this, (array) $value),
+                        default => $property->setValue($this, $value),
+                    };
+
+                    continue;
+                }
+            }
+
+            $property->setValue($this, $value);
+        }
     }
 }
