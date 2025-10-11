e288574163fbb50ae831959074b0fdd1c8ea7f07
aleksandar.naydenov <aleksandar.naydenov@delta.bg>
2025-10-11 16:05:10 +0300

    Update composer.json and GrenadeType.php to add new test scripts and enforce return types for methods

--- src/Enums/GrenadeType.php
+++ src/Enums/GrenadeType.php
@@ -12,17 +12,17 @@ enum GrenadeType: string
     case FRAG = 'frag';
     case DECOY = 'decoy';
 
-    public function isDecoySmoke()
+    public function isDecoySmoke(): bool
     {
         return $this === self::DECOY || $this === self::SMOKE;
     }
 
-    public function isDefault()
+    public function isDefault(): bool
     {
         return $this === self::FRAG || $this === self::FLASH_BANG;
     }
 
-    public function isFireBomb()
+    public function isFireBomb(): bool
     {
         return $this === self::INFERNO;
     }
